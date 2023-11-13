213467d5deee92dc95edd1cbf9f9468ecae72066
DenisGitHubs <128564592+DenisGitHubs@users.noreply.github.com>
2023-11-13 19:09:31 +0700

    profile page

---
+++ src/pages/profile/profile.jsx
@@ -0,0 +1,86 @@
+import * as S from "./profileStyle";
+import { useState } from "react";
+import { Header } from "../../Components/header/header";
+
+export default function Profile() {
+  const [edit, setEdit] = useState(false);
+  const [editPass, setEditPass] = useState(false);
+  const [valuePass, setValuePass] = useState(null);
+
+
+  return (
+    <S.Container>
+      <S.Content>
+      <Header />
+        <S.SubTitle>
+          <S.TitleText>Мой профиль</S.TitleText>
+          <S.TitleTextLogin>
+            Логин:<S.Text>user@test.com</S.Text>
+          </S.TitleTextLogin>
+          {edit ? (
+            <div>
+              <S.UserLogin/>
+              <S.LoginButton >Сохранить</S.LoginButton>
+              <S.LoginButton onClick={() => setEdit(false)}>
+                Отмена
+              </S.LoginButton>
+            </div>
+          ) : null}
+          <S.TitleTextPass>
+            Пароль:<S.Text>password</S.Text>
+          </S.TitleTextPass>
+          {editPass ? (
+            <div>
+              <S.UserLogin
+                onChange={(e) => {
+                  setValuePass(e.target.value);
+                }}
+                value={valuePass}
+              />
+              <S.LoginButton>
+                Сохранить
+              </S.LoginButton>
+              <S.LoginButton onClick={() => setEditPass(false)}>
+                Отмена
+              </S.LoginButton>
+            </div>
+          ) : null}
+        </S.SubTitle>
+        <S.ChangeLogPass>
+          <S.LogButton onClick={() => setEdit(true)}>
+            Редактировать логин
+          </S.LogButton>
+
+          <S.PassButton onClick={() => setEditPass(true)}>
+            Редактировать пароль
+          </S.PassButton>
+        </S.ChangeLogPass>
+        <S.TitleCourse>Мои курсы</S.TitleCourse>
+          <S.SportChoice>
+              <li>
+                <S.Sport>
+                  <S.ProfCard
+                    src="/img/profCard1.png"
+                    alt="prof_card"
+                  />
+                  <S.SportButton>
+                    Перейти →
+                  </S.SportButton>
+                </S.Sport>
+              </li>
+            
+          </S.SportChoice>
+      </S.Content>
+    </S.Container>
+  );
+}
+
+
+// (
+//     <div>
+//       <S.textNoPay>У вас ещё не куплено ни одного курса</S.textNoPay>
+//       <Link to="/">
+//         <S.buttonNextPay>Купить...</S.buttonNextPay>
+//       </Link>
+//     </div>
+//   )
--- src/pages/routers/routers.jsx
+++ src/pages/routers/routers.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import { Main } from "../main/main";
 import { Login } from "../login";
 import Course from "../courses/course";
+import Profile from "../profile/profile";
 export const AppRoutes = () => {
     return (
     <>
@@ -9,6 +10,7 @@ export const AppRoutes = () => {
             <Route path="/" element={<Main />} />
             <Route path="/login" element={<Login />} />
             <Route path="/course" element={<Course />} />
+            <Route path="/profile" element={<Profile />} />
     </Routes>
     </>
     );
