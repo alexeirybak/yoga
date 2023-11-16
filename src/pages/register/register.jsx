18641d61064d69c1ebb6dc46566d6cd4736d34d7
alexeirybak <rybak2108@yandex.ru>
2023-11-16 21:31:47 +0300

    A bit fixed

--- src/pages/register/register.jsx
+++ src/pages/register/register.jsx
@@ -1,33 +1,28 @@
-import * as S from './styles';
+import * as S from "./styles";
 
 export function Register() {
-
   return (
     <S.Wrapper>
       <S.ContainerEnter>
-      <S.ModalFormLoginTopImg src="/logo-top.png" alt="logo" />
+        <S.ModalFormLoginTopImg src="/logo-top.png" alt="logo" />
         <S.ModalBlock>
-          <S.ModalFormLogin action='#'>
-            <S.ModalFormLoginLink href='../'>
+          <S.ModalFormLogin action="#">
             <S.ModalFormLoginImg src="/logo.png" alt="logo" />
-            </S.ModalFormLoginLink>
             <S.ModalFormLoginInput>
-              <S.ModalInput type='text' name='login' placeholder='Почта' />
+              <S.ModalInput type="text" name="login" placeholder="Почта" />
               <S.ModalInput
-                type='password'
-                name='password'
-                placeholder='Пароль'
+                type="password"
+                name="password"
+                placeholder="Пароль"
               />
-                <S.ModalInputConfirmPassword
-                  type='password'
-                  name='password'
-                  placeholder='Повторите пароль'
-                />
+              <S.ModalInputConfirmPassword
+                type="password"
+                name="password"
+                placeholder="Повторите пароль"
+              />
             </S.ModalFormLoginInput>
             <S.ModalFormLoginButtons>
-              <S.ModalBtnSignup>
-                Зарегистрироваться
-              </S.ModalBtnSignup>
+              <S.ModalBtnSignup>Зарегистрироваться</S.ModalBtnSignup>
             </S.ModalFormLoginButtons>
           </S.ModalFormLogin>
         </S.ModalBlock>
