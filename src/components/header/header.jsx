ce20e83456c395a54033da7ad5af0dcfb51cc028
DenisGitHubs <128564592+DenisGitHubs@users.noreply.github.com>
2023-11-20 21:19:50 +0700

    3

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -4,14 +4,16 @@ import * as S from "./headerStyle";
 import { useSelector } from "react-redux";
 import { useAuth } from "../../hooks/use-auth";
 
+
 export function Header() {
-  const {isAuth, email} = useAuth();
+  const logo = useSelector(state => state.logo)
   const navigate = useNavigate();
   const [visible, setVisible] = useState(true);
-  const user = useSelector(state => state.user);
-  const logo = useSelector(state => state.logo)
-  const toggleVisibility = () => setVisible(!visible);
+  const {isAuth, email} = useAuth();
 
+  let user = 'UserName';
+
+  const toggleVisibility = () => setVisible(!visible);
   const ToMain = () => {
     navigate("/");
   }
@@ -25,32 +27,31 @@ export function Header() {
   return (
     <>
         <S.HeadContentDiv>
-          {isAuth ?           
-          <S.UserDiv>
-            <Link to="/profile">
-              <S.UserPhotoImg src="/img/Ellipse.png" alt="userphoto" />
+          <S.LogoTitleDiv>
+            <Link to="/">
+            {logo.logo === "white" ? <S.LogoImg src="/img/logo.png" alt="logo" /> : <S.LogoImg src="/img/logoBlack.png" alt="logo" />}
             </Link>
-            <S.UserNameSpan onClick={toggleVisibility}>
+          </S.LogoTitleDiv>
+          <S.UserDiv>
+          {isAuth ?<>
+              <S.UserPhotoImg src="/img/Ellipse.png" alt="userphoto" />
+              <S.UserNameSpan color={logo.logo} onClick={toggleVisibility}>
               {user} ↓
             </S.UserNameSpan>
-            </S.UserDiv> : 
-                    <S.LogoTitle>
-                    <Link to="/">
-                        {logo.logo === "white" ? <S.LogoImg src="/img/logo.png" alt="logo" /> : <S.LogoImg src="/img/logoBlack.png" alt="logo" />}
-                    </Link>
-                    <Link to="/login">
-                    <S.Enter>Войти</S.Enter>
-        </Link>
-                      </S.LogoTitle>  
+            </>
+             :
+                        <Link to="/login">
+                        <S.EnterButton  >Войти</S.EnterButton>
+                      </Link>
             }
-
+            </S.UserDiv>
             </S.HeadContentDiv>
             {!visible && (
                             <S.HeaderList>
             <S.HeaderUl>
-            <S.HeaderLi onClick={ToMain}>На главную</S.HeaderLi>
-            <S.HeaderLi onClick={ToProfile}>Профиль</S.HeaderLi>
-            <S.HeaderLi onClick={Exit}>Выход</S.HeaderLi>
+            <S.HeaderLi color={logo.logo} onClick={ToMain}>На главную</S.HeaderLi>
+            <S.HeaderLi color={logo.logo} onClick={ToProfile}>Профиль</S.HeaderLi>
+            <S.HeaderLi color={logo.logo} onClick={Exit}>Выход</S.HeaderLi>
             </S.HeaderUl>
                         </S.HeaderList>
         )}
