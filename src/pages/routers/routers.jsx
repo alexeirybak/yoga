dead4c9f4aa1773f0fbf23a017439703af1eae1e
katerinanosova <katerinanosova@gmail.com>
2023-11-23 22:18:04 +0100

    updated workout selection process

--- src/pages/routers/routers.jsx
+++ src/pages/routers/routers.jsx
@@ -26,7 +26,7 @@ export const AppRoutes = () => {
         <Route element={<ProtectedRoute isAllowed={isAuthorized} />}>
           <Route path="/course/:id" element={<Course />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/training" element={<Trainings />} />
+          <Route path="/training/:id" element={<Trainings />} />
         </Route>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
