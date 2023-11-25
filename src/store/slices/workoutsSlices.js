626bc6531a89fdf58df1a2b2c31396b177750537
DenisGitHubs <128564592+DenisGitHubs@users.noreply.github.com>
2023-11-25 12:38:06 +0700

    1

--- src/store/slices/workoutsSlices.js
+++ src/store/slices/workoutsSlices.js
@@ -1,29 +1,33 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-    name: null,
-    type: null,
-    video_file: null,
-    isFinished: null,
-    exercise: [],
-    id: null,
+// const initialState = {
+//     name: null,
+//     type: null,
+//     video_file: null,
+//     isFinished: null,
+//     exercise: [],
+//     id: null,
+// };
 
+const initialState = {
+trainingData: [],
 };
 
 const workoutSlice = createSlice({
-  name: "workout",
+  name: "workouts",
   initialState,
   reducers: {
     setWorkout(state, action) {
-        state.name = action.payload.name;
-        state.type = action.payload.type;
-        state.video_file = action.payload.video_file;
-        state.isFinished = action.payload.isFinished;
-        state.exercise = action.payload.exercise;
-        state.id = action.payload.id;
+        state.trainingData = action.payload;
     },
   },
 });
 
 export const { setWorkout } = workoutSlice.actions;
 export default workoutSlice.reducer;
+
+// state.type = action.payload.type;
+// state.video_file = action.payload.video_file;
+// state.isFinished = action.payload.isFinished;
+// state.exercise = action.payload.exercise;
+// state.id = action.payload.id;
